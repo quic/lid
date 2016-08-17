f0c1f3b8c2a910fb05b285d581f4a3c2929ba3f6
Andrew Leverentz <aleveren@codeaurora.org>
2016-08-17 23:34:06 +0000

    Add optional ability to return score "rationale"

--- license_identifier/prep.py
+++ license_identifier/prep.py
@@ -11,11 +11,103 @@ DEFAULT_PICKLE_PROTOCOL_VERSION = 2
 
 _nltk_tokenizer = nltk.tokenize.WordPunctTokenizer()
 
-def _tokenize(text):
+def _span_tokenize(text):
     if not isinstance(text, six.string_types):
         # Interpret text as a list of lines
         text = '\n'.join(text)
-    return _nltk_tokenizer.tokenize(text)
+    return list(_nltk_tokenizer.span_tokenize(text))
+
+def _tokens_and_positions_by_line(lines):
+    token_positions_by_line = [_span_tokenize(line) for line in lines]
+    tokens_by_line = []
+    for i in xrange(len(lines)):
+        current_tokens = []
+        for start, end in token_positions_by_line[i]:
+            current_tokens.append(lines[i][start:end])
+        tokens_by_line.append(current_tokens)
+    return tokens_by_line, token_positions_by_line
+
+
+class TokenLocatorMixin(object):
+    '''
+    License and Source inherit from this class in order to share
+    token-locating functionality.
+    The subclass must provide the attribute `token_positions_by_line`.
+    '''
+
+    def _locate_token(self, token_index):
+        if token_index < 0:
+            raise ValueError("Token index is negative")
+        current_token_index = 0
+        for line_index, positions in enumerate(self.token_positions_by_line):
+            if token_index - current_token_index >= len(positions):
+                current_token_index += len(positions)
+                continue
+            start_in_line, end_in_line = \
+                positions[token_index - current_token_index]
+            return (line_index, start_in_line, end_in_line)
+        raise ValueError("Token index is too large")
+
+    def _get_ignored_text_before_token(self, token_index):
+        '''
+        Extracts the "ignored" text (generally whitespace) that appears
+        before the given `token_index`.  If the index is 0, this will
+        extract any initial whitespace, and if the index is equal to the
+        total number of tokens, this will extract any final whitespace.
+        Otherwise, this will extract the whitespace that appears after
+        (token_index - 1) and before (token_index).
+        '''
+        total_num_tokens = sum(len(t_list) for t_list in self.token_positions_by_line)
+        if token_index < 0:
+            raise ValueError("Token index is negative")
+        if token_index > total_num_tokens:
+            raise ValueError("Token index is too large")
+
+        if total_num_tokens == 0:
+            # Return full text if there are no tokens
+            return '\n'.join(self.lines) + '\n'
+
+        # Locate the boundaries of the ignored text
+        if token_index == 0:
+            start_line_index = 0
+            char_offset_within_start_line = 0
+        else:
+            line_index, start, end = self._locate_token(token_index - 1)
+            start_line_index = line_index
+            char_offset_within_start_line = end
+
+        if token_index == total_num_tokens:
+            end_line_index = len(self.lines) - 1
+            char_offset_within_end_line = len(self.lines[end_line_index])
+        else:
+            line_index, start, end = self._locate_token(token_index)
+            end_line_index = line_index
+            char_offset_within_end_line = start
+
+        # Extract the ignored text
+        ignored_text = ''
+        for line_index in range(start_line_index, end_line_index + 1):
+            line = self.lines[line_index]
+
+            if line_index == start_line_index:
+                start_within_line = char_offset_within_start_line
+            else:
+                start_within_line = 0
+                # Insert a newline for all but the first line
+                ignored_text += '\n'
+
+            if line_index == end_line_index:
+                end_within_line = char_offset_within_end_line
+            else:
+                end_within_line = len(line)
+
+            ignored_text += line[start_within_line : end_within_line]
+
+        # Ignored text after final token should include a newline
+        if token_index == total_num_tokens:
+            ignored_text += '\n'
+
+        return ignored_text
 
 
 class License(namedtuple("License",
@@ -24,13 +116,17 @@ class License(namedtuple("License",
          "lines",
          "offsets_by_line",
          "tokens",
-         "n_grams"])):
+         "token_positions_by_line",
+         "n_grams"]), TokenLocatorMixin):
 
     @staticmethod
     def from_filename(filename):
         name, ext = os.path.splitext(os.path.basename(filename))
         lines, offsets_by_line = util.read_lines_offsets(filename)
-        tokens = _tokenize(lines)
+        tokens_by_line, token_positions_by_line = _tokens_and_positions_by_line(lines)
+        tokens = []
+        for token_list in tokens_by_line:
+            tokens.extend(token_list)
         n_grams = ng.n_grams(lines)
         return License(
             name = name,
@@ -38,12 +134,16 @@ class License(namedtuple("License",
             lines = lines,
             offsets_by_line = offsets_by_line,
             tokens = tokens,
+            token_positions_by_line = token_positions_by_line,
             n_grams = n_grams)
 
     @staticmethod
     def from_lines(lines, name = "<from_lines>"):
         lines, offsets_by_line = util.get_lines_and_line_offsets(lines)
-        tokens = _tokenize(lines)
+        tokens_by_line, token_positions_by_line = _tokens_and_positions_by_line(lines)
+        tokens = []
+        for token_list in tokens_by_line:
+            tokens.extend(token_list)
         n_grams = ng.n_grams(lines)
         return License(
             name = name,
@@ -51,6 +151,7 @@ class License(namedtuple("License",
             lines = lines,
             offsets_by_line = offsets_by_line,
             tokens = tokens,
+            token_positions_by_line = token_positions_by_line,
             n_grams = n_grams)
 
 
@@ -59,30 +160,35 @@ class Source(namedtuple("Source",
          "lines",
          "original_line_offset",
          "offsets_by_line",
-         "tokens_by_line"])):
+         "token_positions_by_line",
+         "tokens_by_line"]), TokenLocatorMixin):
 
     @staticmethod
     def from_filename(filename):
         lines, offsets_by_line = util.read_lines_offsets(filename)
-        tokens_by_line = [_tokenize(line) for line in lines]
+        tokens_by_line, token_positions_by_line = \
+            _tokens_and_positions_by_line(lines)
 
         return Source(
             filename = filename,
             lines = lines,
             original_line_offset = 0,
             offsets_by_line = offsets_by_line,
+            token_positions_by_line = token_positions_by_line,
             tokens_by_line = tokens_by_line)
 
     @staticmethod
     def from_lines(lines):
         lines, offsets_by_line = util.get_lines_and_line_offsets(lines)
-        tokens_by_line = [_tokenize(line) for line in lines]
+        tokens_by_line, token_positions_by_line = \
+            _tokens_and_positions_by_line(lines)
 
         return Source(
             filename = None,
             lines = lines,
             original_line_offset = 0,
             offsets_by_line = offsets_by_line,
+            token_positions_by_line = token_positions_by_line,
             tokens_by_line = tokens_by_line)
 
     def subset(self, start_ind, end_ind):
@@ -91,6 +197,7 @@ class Source(namedtuple("Source",
             lines = self.lines[start_ind : end_ind],
             original_line_offset = self.original_line_offset + start_ind,
             offsets_by_line = self.offsets_by_line[start_ind : end_ind],
+            token_positions_by_line = self.token_positions_by_line[start_ind : end_ind],
             tokens_by_line = self.tokens_by_line[start_ind : end_ind])
 
 
--- license_identifier/prep_test.py
+++ license_identifier/prep_test.py
@@ -1,21 +1,35 @@
 from . import prep
 
 import os
+import pytest
 
 
 BASE_DIR = os.path.join(os.path.dirname(__file__), "..")
 
-def test_tokenize():
-    result = prep._tokenize(["Testing, test-ing: 1 - 2 - 3."])
-    assert result == ["Testing", ",", "test", "-", "ing", ":", "1", "-", "2", "-", "3", "."]
+def test_span_tokenize():
+    result = prep._span_tokenize("a bc\nde f")
+    expected = [(0,1), (2,4), (5,7), (8,9)]
+    assert result == expected
 
-    result = prep._tokenize("Testing, again.")
-    assert result == ["Testing", ",", "again", "."]
+    result = prep._span_tokenize(["a bc", "de f"])
+    assert result == expected
+
+def test_tokens_and_positions_by_line():
+    tok, pos = prep._tokens_and_positions_by_line(["a bc", "de f"])
+    assert tok == [["a", "bc"], ["de", "f"]]
+    assert pos == [[(0,1), (2,4)], [(0,2), (3,4)]]
 
 def test_prep_license():
     path = os.path.join(BASE_DIR, "data", "test", "license", "test_license.txt")
     lic = prep.License.from_filename(path)
     assert len(lic.lines) == 1
+
+    lic = prep.License.from_lines(["  ab  cd  ", "", "   ef   "])
+    assert len(lic.lines) == 3
+    assert lic._locate_token(1) == (0, 6, 8)
+
+    lic = prep.License.from_lines(["  ab c,d.  ", "\t", "   ef   "])
+    assert lic._get_ignored_text_before_token(5) == "  \n\t\n   "
 
 def test_prep_source():
     path = os.path.join(BASE_DIR, "data", "test", "data", "test1.py")
@@ -33,6 +47,41 @@ def test_prep_source():
     src_subset_3 = src_subset_2.subset(1, 2)
     assert src_subset_3.lines == ["seven"]
     assert src_subset_3.original_line_offset == 4
+
+def test_source_locate_token():
+    src = prep.Source.from_lines(["  ab  cd  ", "", "   ef   "])
+    assert src.tokens_by_line == [["ab", "cd"], [], ["ef"]]
+    assert src.token_positions_by_line == [[(2,4), (6,8)], [], [(3,5)]]
+    assert src._locate_token(0) == (0, 2, 4)
+    assert src._locate_token(1) == (0, 6, 8)
+    assert src._locate_token(2) == (2, 3, 5)
+
+    with pytest.raises(ValueError):
+        src._locate_token(-1)
+    with pytest.raises(ValueError):
+        src._locate_token(3)
+
+def test_source_get_ignored_text_before_token():
+    src = prep.Source.from_lines(["  ab c,d.  ", "\t", "   ef   "])
+    assert src.tokens_by_line == [["ab", "c", ",", "d", "."], [], ["ef"]]
+    assert src._get_ignored_text_before_token(0) == "  "
+    assert src._get_ignored_text_before_token(1) == " "
+    assert src._get_ignored_text_before_token(2) == ""
+    assert src._get_ignored_text_before_token(3) == ""
+    assert src._get_ignored_text_before_token(4) == ""
+    assert src._get_ignored_text_before_token(5) == "  \n\t\n   "
+    assert src._get_ignored_text_before_token(6) == "   \n"
+
+    with pytest.raises(ValueError):
+        src._get_ignored_text_before_token(-1)
+    with pytest.raises(ValueError):
+        src._get_ignored_text_before_token(7)
+
+    src = prep.Source.from_lines([])
+    assert src._get_ignored_text_before_token(0) == "\n"
+
+    src = prep.Source.from_lines(["   ", "  ", " "])
+    assert src._get_ignored_text_before_token(0) == "   \n  \n \n"
 
 def test_license_library():
     license_dir = os.path.join(BASE_DIR, "data", "test", "license")
