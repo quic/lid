080163706c64e2628d5f9c980b6c9c2721637d9e
Rashmi Chitrakar <rashmic@codeaurora.org>
2016-08-26 16:58:01 -0700

    Tag 0.5.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class CustomInstall(install):
 
 setup(
     name='license_identifier',
-    version='0.5.0',
+    version='0.5.1',
     description='Scans a file or folder for predefined licenses',
     author='Peter Shin',
     author_email='phshin@qti.qualcomm.com',
