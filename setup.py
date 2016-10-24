b27862568f5b0f0dd847199e38548209129a1edf
Jesse Porter <jporter@codeaurora.org>
2016-10-23 23:48:15 -0700

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 
+import os
+
 from setuptools import setup
 from setuptools.command.install import install
 
-import os
 
 class CustomInstall(install):
+
     def run(self):
         # Perform original install steps
         install.run(self)
 
         # Perform custom install steps
         from license_identifier.license_identifier import LicenseIdentifier
-        license_dir = os.path.join(self.install_lib, 'license_identifier/data/license_dir')
-        pickle_file_path = os.path.join(self.install_lib, 'license_identifier/data/license_n_gram_lib.pickle')
-        lcs_id_obj = LicenseIdentifier(
-            license_dir = license_dir,
-            pickle_file_path = pickle_file_path)
+
+        license_dir = os.path.join(
+            self.install_lib, 'license_identifier/data/license_dir'
+        )
+        pickle_file_path = os.path.join(
+            self.install_lib,
+            'license_identifier/data/license_n_gram_lib.pickle'
+        )
+
+        LicenseIdentifier(license_dir=license_dir,
+                          pickle_file_path=pickle_file_path)
+
 
 setup(
     name='license_identifier',
