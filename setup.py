f945c5302de6e74b78d3eb810171277f0d245bf2
Rashmi Chitrakar <rashmic@codeaurora.org>
2016-12-20 11:12:33 -0800

    Upgrade License Identifier to 1.1.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class CustomInstall(install):
 
 setup(
     name='license_identifier',
-    version='1.1.1',
+    version='1.1.2',
     description='Scans a file or folder for predefined licenses',
     author='Peter Shin',
     author_email='phshin@qti.qualcomm.com',
