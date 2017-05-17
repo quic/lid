5e5dc9102dcb337cfdc0bec0576394097ad70bec
Jesse Porter <jporter@codeaurora.org>
2017-05-17 15:48:19 -0700

    Bump version to 1.2.4

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ class CustomInstall(install):
 
 setup(
     name='lid',
-    version='1.2.3',
+    version='1.2.4',
     description='Scans a file or folder for predefined licenses',
     author='Peter Shin',
     author_email='phshin@codeaurora.org',
