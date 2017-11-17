61bc0d09cc4a7fa162fac98a836cdc86d24bb281
Jesse Porter <jporter@codeaurora.org>
2017-11-17 13:17:01 -0800

    Tag 1.2.9
    Signed-off-by: Craig Northway <cnorthway@codeuarora.org>

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ class CustomInstall(install):
 
 setup(
     name='lid',
-    version='1.2.8',
+    version='1.2.9',
     description='Scans a file or folder for predefined licenses',
     author='Peter Shin',
     author_email='phshin@codeaurora.org',
