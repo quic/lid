f149884a7dc2354a6e52a723e6ea596a4c436ef4
Craig Northway <cnorthway@codeaurora.org>
2019-01-08 15:55:53 -0800

    Update version to 1.4.3

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ class CustomInstall(install):
 
 setup(
     name='lid',
-    version='1.4.1',
+    version='1.4.3',
     description='Scans a file or folder for predefined licenses',
     author='Craig Northway',
     author_email='craign@qti.qualcomm.com',
